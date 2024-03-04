1aa4be0b5b49210734dfcb45dae5c550edc24a95
Nia447 <bayanshonhodoev@mail.ru>
2024-03-05 00:02:37 +0700

    Add layers Domain, Application, Persistence, Infrastructure

---
+++ backend/WebApiApplications/InternetBank.Core.Infrastructure/Extensions/InfrastructureServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InternetBank.Core.Infrastructure.Extensions;
+
+public static class InfrastructureServiceCollectionExtension
+{
+    public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddServices();
+    }
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        // TODO: Add services
+    }
+}
---
+++ backend/WebApiApplications/InternetBank.Core.Persistence/Extensions/PersistenceServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InternetBank.Core.Persistence.Extensions;
+
+public static class PersistenceServiceCollectionExtension
+{
+    public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddServices();
+    }
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        // TODO: Add services
+    }
+}
