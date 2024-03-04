fad8c8c727c74ec0e0d7f7319037adb632a58c39
Nia447 <bayanshonhodoev@mail.ru>
2024-03-05 00:06:47 +0700

    Add entities of pattern UnitOfWork and Repository in Application layer

---
+++ backend/WebApiApplications/InternetBank.Core.Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using InternetBank.Core.Domain.Common;
+
+namespace InternetBank.Core.Application.Interfaces.Repositories;
+
+public interface IUnitOfWork
+{
+    IGenericRepository<T> Repository<T>() where T : BaseAuditableEntity;
+    Task<int> Save(CancellationToken cancellationToken);
+    Task<int> SaveAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
+    Task RollBack();
+}
