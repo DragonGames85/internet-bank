a7689809c415c97be20856cb07fa87351f80c996
dragongames85 <dragongames85@mail.ru>
2024-05-07 16:35:37 +0500

    ADD WEB-MONITOR

---
+++ web-monitor/app/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { useState } from 'react';
+import { faker } from '@faker-js/faker';
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+import { Line } from 'react-chartjs-2';
+
+export default function Home() {
+    const [statusCodes, setStatusCodes] = useState<number[]>([]);
+    const [responseTimes, setResponseTimes] = useState<number[]>([]);
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'График стабильности',
+            },
+        },
+    };
+
+    const getRandomValue = () => {
+        const randomStatus = faker.datatype.number({ min: 200, max: 500 });
+        setStatusCodes(prev => [...prev, randomStatus]);
+        setResponseTimes(prev => {
+            if (prev.length == 0) {
+                return [1];
+            } else {
+                return [...prev, prev[prev.length - 1] + 1];
+            }
+        });
+    };
+
+    const data = {
+        labels: responseTimes,
+        datasets: [
+            {
+                label: 'СТАТУС КОД',
+                data: statusCodes,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between px-12 py-2">
+            <Line options={options} data={data} />
+            <button
+                onClick={getRandomValue}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Мокнуть запрос
+            </button>
+        </main>
+    );
+}
